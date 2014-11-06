c7e5eaf72098b1075b0f3817554ff48a63ed8850
Jiao Xianjun <putaoshu@gmail.com>
2014-11-06 09:05:09 +0800

    shorten signal in LTE_DL_receiver by hand

--- Matlab/LTE_DL_receiver.m
+++ Matlab/LTE_DL_receiver.m
@@ -107,12 +107,12 @@ if isempty(dir([filename(1:end-4) '.mat'])) || nargin == 3
 
     figure(1);
 %     show_signal_time_frequency(r_20M, sampling_rate, 180e3);
-    show_signal_time_frequency(r_raw(1 : (15e-3*raw_sampling_rate)), raw_sampling_rate, 50e3);
+    show_signal_time_frequency(r_raw(1 : (25e-3*raw_sampling_rate)), raw_sampling_rate, 50e3);
     figure(2);
-    show_time_frequency_grid_raw(r_raw(1 : (15e-3*raw_sampling_rate)), raw_sampling_rate);
+    show_time_frequency_grid_raw(r_raw(1 : (25e-3*raw_sampling_rate)), raw_sampling_rate);
     
-    r_pbch = filter_wo_tail(r_raw, coef_pbch.*5, sampling_rate_pbch/raw_sampling_rate);
-    r_20M = filter_wo_tail(r_raw, coef_8x_up.*8, 8);
+    r_pbch = filter_wo_tail(r_raw(1 : (80e-3*raw_sampling_rate)), coef_pbch.*5, sampling_rate_pbch/raw_sampling_rate);
+    r_20M = filter_wo_tail(r_raw(1 : (80e-3*raw_sampling_rate)), coef_8x_up.*8, 8);
     r_20M = r_20M(1:5:end);
     
     
